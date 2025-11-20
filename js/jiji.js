10f4d9147dde22dd052558af3e61f88d7ab1da4c
cmbf2023-dev <cmbf2023@gmail.com>
2025-11-20 01:34:17 -0800

    Update jiji.js

--- js/jiji.js
+++ js/jiji.js
@@ -29,39 +29,7 @@ for( let x = 0; x < select3.length; x++ ){
 	select3[x].style.display = "none";
 }
 
-const nameInterval = setInterval(()=>{
-const name = document.querySelector("#__nuxt > div > div.b-body-wrapper.js-body-wrapper > div > div > div > div > div.b-seller-page > div > div > div.b-seller-mobile-info-block > div.b-seller-mobile-info-block__header.h-dflex.h-mb-15 > div.b-seller-mobile-info-block__header-text > div.b-seller-mobile-info-block__header-name.h-mb-5 > span");
 
-
-const select4 = document.querySelectorAll(".fw-button--type-bloated-success");
-
-for( let x = 0; x < select4.length; x++ ){
-	select4[x].style.backgroundColor = "#032b2e";
-}
-const heInterval = setInterval(()=>{
-let he = document.querySelector(".fw-button__slot-wrapper.fw-button__text--has-icon");
-
-if(he) {
-    // Clone the element (true = deep clone including child elements)
-	let isCloned = he.getAttribute("data-cloned");
-
-	if(isCloned ){
-		clearInterval(heInterval);
-	}
-    let clonedElement = he.cloneNode(true);
-    
-    // Modify the text content
-    clonedElement.innerText = "Bid Now";
-	clonedElement.addEventListener("click", function(){
-		showBiddingPopup();
-	});
-
-	clonedElement.setAttribute("data-cloned", "true");
-    
-    // Replace the original element with the cloned one
-    he.parentNode.replaceChild(clonedElement, he);
-}
-}, 1500);
 
 
 const bodyInterval = setInterval(()=>{
