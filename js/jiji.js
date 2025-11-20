f6edd6a1bfc916849b3867e38c2521a4bdd1a801
cmbf2023-dev <cmbf2023@gmail.com>
2025-11-19 17:13:37 -0800

    Update jiji.js

--- js/jiji.js
+++ js/jiji.js
@@ -3,6 +3,12 @@ const select = document.querySelectorAll(".h-bg-jiji-green");
 for( let x = 0; x < select.length; x++ ){
 	select[x].style.backgroundColor = "#032b2e";
 }
+
+const select2 = document.querySelectorAll(".b-seller-page-header__button");
+
+for( let x = 0; x < select2.length; x++ ){
+	select2[x].style.backgroundColor = "#032b2e";
+}
 const bodyInterval = setInterval(()=>{
 	const root = document.body;
 	if(root ){
