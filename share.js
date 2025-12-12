bf087f1ed30bb2eae1a71502e03777b05e9e85fc
cmbf2023-dev <cmbf2023@gmail.com>
2025-12-12 11:49:31 +0100

    Update share.js

--- share.js
+++ share.js
@@ -1,5 +1,5 @@
 /* console.log('>>> start'); */
-importScripts('https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm');
+importScripts('https://cdn.skypack.dev/@supabase/supabase-js@2');
 
 const SUPABASE_URL = "https://svtbqnysmjffbstuotwd.supabase.co"
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2dGJxbnlzbWpmZmJzdHVvdHdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2OTE4NDcsImV4cCI6MjA3OTI2Nzg0N30.5DTPDygrRnQDW5W-NadS7cYr_PmQuVGC5K8BXWBsqtQ"
